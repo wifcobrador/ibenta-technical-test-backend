56e70c988524967aa5fcc97db66d608ffec80a8b
Nando Dela Cruz <sesinando2@gmail.com>
2020-05-05 00:46:41 +1000

    Added object mapper

--- src/main/java/au/com/ibenta/config/WebConfiguration.java
+++ src/main/java/au/com/ibenta/config/WebConfiguration.java
@@ -1,7 +1,10 @@
 package au.com.ibenta.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -16,5 +19,14 @@ public class WebConfiguration {
         final var source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
         return new CorsWebFilter(source);
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder configureObjectMapper() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        builder.configure(objectMapper);
+        return builder;
     }
 }
