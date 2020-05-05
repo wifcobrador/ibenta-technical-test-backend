007bf00a32d3a71ffb74d0b9587326fa719b17fd
Nando Dela Cruz <sesinando2@gmail.com>
2020-05-05 19:54:27 +1000

    Updated checkstyle and spotbugs config

--- src/main/java/au/com/ibenta/template/TemplateErrorController.java
+++ src/main/java/au/com/ibenta/template/TemplateErrorController.java
@@ -2,8 +2,6 @@ package au.com.ibenta.template;
 
 import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
